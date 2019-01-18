16a255549ca2b3b7bcf4fe4b13d6e3847d9a203a
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-18 10:32:02 -0800

    add startButton functionality

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("#startButton").click(function(event){
+    var html = 0;
+    var cSharp = 0;
+    var java = 0;
+    var php = 0;
+    var ruby = 0;
+
+    $("#question1").show();
+    $("#startButton").hide();
+
+    (event).preventDefault();
+  });
+});
